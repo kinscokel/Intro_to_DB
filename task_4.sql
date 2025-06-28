a7ffdf9a63122b74efd8012e130c735ed343d29f
kingsley <kinscoco9@gmail.com>
2025-06-28 16:47:09 +0100

    committing my file

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,3 @@
 USE alx_book_store;
--- show the full table books from the database 
-print(" show full table books from database")
+-- show the full content of table books from the database 
+print(" show full content of the table books from database")
