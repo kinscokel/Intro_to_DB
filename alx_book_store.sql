d8172d6e2a378be618a64e5041f2cea75ec79fd1
kingsley <kinscoco9@gmail.com>
2025-06-28 14:19:42 +0100

    committing my file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,7 +38,7 @@ CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
