a3fa755eb40b1cd133a4e3815e841a8171fd8fc4
kingsley <kinscoco9@gmail.com>
2025-06-28 17:27:02 +0100

    committing my file

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
 INSERT INTO customer(customer_id, customer_name, email, address )
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Avenue.');
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Hapiness Avenue.');
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Hpiness Avenue.')
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Hapiness Ave.');
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Hpiness Ave.')
