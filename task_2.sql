72600abf22235a4a43898a64493d597cbe6b4d67
kingsley <kinscoco9@gmail.com>
2025-06-28 16:02:00 +0100

    committing my file

--- task_2.sql
+++ task_2.sql
@@ -1,3 +1,4 @@
+-- Create Book table
 CREATE TABLE books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     tittle VARCHAR(130),
@@ -6,13 +7,13 @@ CREATE TABLE books(
     publication_date DATE NOT NULL,
 
 );
-
+-- Create Authors table
 CREATE TABLE authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215),
 
 );
-
+-- Create Customers table
 CREATE TABLE customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
@@ -20,14 +21,14 @@ CREATE TABLE customers(
     address TEXT,
 
 );
-
+-- Create Orders table
 CREATE TABLE orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
     order_date DATE NOT NULL,
 
 );
-
+-- Create Order_Details table
 CREATE TABLE Order_Details(
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT FOREIGN KEY(order_id) REFERENCES order(order_id),
